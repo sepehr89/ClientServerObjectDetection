053c445becff1d6bb7bffb0a5bf5e4af80b6672a
sepehr89 <52756289+sepehr89@users.noreply.github.com>
2019-09-07 10:03:10 +0430

    Update yolo_opencv.py

--- scira_client/yolo_opencv.py
+++ scira_client/yolo_opencv.py
@@ -1,10 +1,3 @@
-#############################################
-# Object detection - YOLO - OpenCV
-# Author : Arun Ponnusamy   (July 16, 2018)
-# Website : http://www.arunponnusamy.com
-############################################
-
-
 import cv2
 import argparse
 import numpy as np
